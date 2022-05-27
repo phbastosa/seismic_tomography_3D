473d22d09f7992a4921ee06468b03a4f7f51d6c7
pbastosA <pbastos@id.uff.br>
2022-05-27 15:49:36 -0300

    simbgf example runs automatically

--- src/eikonal/runTest.sh
+++ src/eikonal/runTest.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
-# python3 simbgf2022.py 1
+python3 simbgf2022.py 1
 
-# inout="../essentials/inout/inout.cpp"
-# model="../essentials/model/model.cpp"
-# utils="../essentials/utils/utils.cpp"
-# geom="../essentials/geometry/geometry.cpp"
-# eikonal="eikonal.cpp"
+inout="../essentials/inout/inout.cpp"
+model="../essentials/model/model.cpp"
+utils="../essentials/utils/utils.cpp"
+geom="../essentials/geometry/geometry.cpp"
+eikonal="eikonal.cpp"
 
-# flags="-fast -acc -ta=tesla,cc60 -std=c++11 -g -lm"
+flags="-fast -acc -ta=tesla,cc60 -std=c++11 -g -lm"
 
-# pgc++ $inout $utils $model $geom $eikonal eikonalAccuracyStudy.cpp $flags -o test.exe
+pgc++ $inout $utils $model $geom $eikonal eikonalAccuracyStudy.cpp $flags -o test.exe
 
-# ./test.exe
+./test.exe
 
-# rm *.o *.exe
+rm *.o *.exe
 
-# for s in 1 2 3 4
-# do
-#     for i in 100 50 25
-#     do
-#         if [ $s = "1" ]; then
-#             mv "fim_central_${i}m_times_nr5048_shot_${s}.bin" "fim_5_${i}m.bin"
-#             mv "pod_central_${i}m_times_nr5048_shot_${s}.bin" "pod_5_${i}m.bin"
-#         fi
+for s in 1 2 3 4
+do
+    for i in 100 50 25
+    do
+        if [ $s = "1" ]; then
+            mv "fim_central_${i}m_times_nr5048_shot_${s}.bin" "fim_5_${i}m.bin"
+            mv "pod_central_${i}m_times_nr5048_shot_${s}.bin" "pod_5_${i}m.bin"
+        fi
         
-#         mv "fim_extern_${i}m_times_nr5048_shot_${s}.bin" "fim_${s}_${i}m.bin"    
-#         mv "pod_extern_${i}m_times_nr5048_shot_${s}.bin" "pod_${s}_${i}m.bin"    
-#     done
-# done
+        mv "fim_extern_${i}m_times_nr5048_shot_${s}.bin" "fim_${s}_${i}m.bin"    
+        mv "pod_extern_${i}m_times_nr5048_shot_${s}.bin" "pod_${s}_${i}m.bin"    
+    done
+done
 
 python3 simbgf2022.py 0
--- src/eikonal/simbgf2022.py
+++ src/eikonal/simbgf2022.py
@@ -60,118 +60,117 @@ else:
     nsrc = len(sx)
     nrec = len(rx)
 
-    # vmin = np.min(model)
-    # vmax = np.max(model)
+    vmin = np.min(model)
+    vmax = np.max(model)
 
-    # xloc = np.linspace(0, nx[n]-1, 7, dtype = int)
-    # xlab = np.array(xloc * dh[n], dtype = int)
+    xloc = np.linspace(0, nx[n]-1, 7, dtype = int)
+    xlab = np.array(xloc * dh[n], dtype = int)
 
-    # yloc = np.linspace(0, ny[n]-1, 7, dtype = int)
-    # ylab = np.array(yloc * dh[n], dtype = int)
+    yloc = np.linspace(0, ny[n]-1, 7, dtype = int)
+    ylab = np.array(yloc * dh[n], dtype = int)
 
-    # zloc = np.linspace(0, nz[n]-1, 5, dtype = int)
-    # zlab = np.array(zloc * dh[n], dtype = int)
+    zloc = np.linspace(0, nz[n]-1, 5, dtype = int)
+    zlab = np.array(zloc * dh[n], dtype = int)
 
-    # N = 10
-    # cmap = plt.get_cmap('viridis',N)
-    # norm = mpl.colors.Normalize(vmin = np.min(times), vmax = np.max(times))
-    # sm = plt.cm.ScalarMappable(cmap = cmap, norm = norm)
-    # sm.set_array([])
+    N = 10
+    cmap = plt.get_cmap('viridis',N)
+    norm = mpl.colors.Normalize(vmin = np.min(times), vmax = np.max(times))
+    sm = plt.cm.ScalarMappable(cmap = cmap, norm = norm)
+    sm.set_array([])
 
-    # plt.figure(1, figsize = (10,10))
+    plt.figure(1, figsize = (10,10))
     
-    # G = gds.GridSpec(4,3)
+    G = gds.GridSpec(4,3)
 
-    # ax1 = plt.subplot(G[:2,:-1]) #-----------------------------------------------------
-    # plt.contour(times[xyPlane,:,:], levels = 10)
-    # cbar = plt.colorbar(sm, cmap=cmap, ticks = np.linspace(0,np.max(times),N), aspect=25)
-    # cbar.set_label("Travel times [s]", fontsize=12)
-    # plt.imshow(model[xyPlane,:,:], aspect="auto", cmap="Greys", vmin=vmin, vmax=vmax)
-    # plt.title("Acquisition geometry", fontsize=20)
-    # plt.ylabel("X axis [m]", fontsize=17)
-    # plt.xlabel("Y axis [m]", fontsize=17)
-    # plt.scatter(ry/dh[n], rx/dh[n], label = "Receivers")
-    # plt.scatter(sy/dh[n], sx/dh[n], label = "Shots", color = "k")
-    # plt.plot(np.arange(ny[n]),np.ones(ny[n])*yzPlane,"--g",label = "YZ plane projection")
-    # plt.plot(np.ones(nx[n])*xzPlane,np.arange(nx[n]),"--r",label = "XZ plane projection")
-    # plt.yticks(xloc,xlab)
-    # plt.xticks(yloc,ylab)
-    # plt.gca().invert_yaxis()
-    # plt.legend(loc="center left",fontsize=8)
+    ax1 = plt.subplot(G[:2,:-1]) #-----------------------------------------------------
+    plt.contour(times[xyPlane,:,:], levels = 10)
+    cbar = plt.colorbar(sm, cmap=cmap, ticks = np.linspace(0,np.max(times),N), aspect=25)
+    cbar.set_label("Travel times [s]", fontsize=12)
+    plt.imshow(model[xyPlane,:,:], aspect="auto", cmap="Greys", vmin=vmin, vmax=vmax)
+    plt.title("Acquisition geometry", fontsize=20)
+    plt.ylabel("X axis [m]", fontsize=17)
+    plt.xlabel("Y axis [m]", fontsize=17)
+    plt.scatter(ry/dh[n], rx/dh[n], label = "Receivers")
+    plt.scatter(sy/dh[n], sx/dh[n], label = "Shots", color = "k")
+    plt.plot(np.arange(ny[n]),np.ones(ny[n])*yzPlane,"--g",label = "YZ plane projection")
+    plt.plot(np.ones(nx[n])*xzPlane,np.arange(nx[n]),"--r",label = "XZ plane projection")
+    plt.yticks(xloc,xlab)
+    plt.xticks(yloc,ylab)
+    plt.gca().invert_yaxis()
+    plt.legend(loc="center left",fontsize=8)
 
-    # plt.text(-240,nx[n],"a)",fontsize=30)
-    # plt.text(sx[0]/dh[n] + 20, sy[0]/dh[n] + 10, "1", fontsize=20, fontweight="bold")    
-    # plt.text(sx[1]/dh[n] - 50, sy[1]/dh[n] + 10, "2", fontsize=20, fontweight="bold")    
-    # plt.text(sx[2]/dh[n] + 20, sy[2]/dh[n] - 20, "3", fontsize=20, fontweight="bold")    
-    # plt.text(sx[3]/dh[n] - 50, sy[3]/dh[n] - 20, "4", fontsize=20, fontweight="bold")    
-    # plt.text(sx[4]/dh[n] + 10, sy[4]/dh[n] + 10, "5", fontsize=20, fontweight="bold")    
+    plt.text(-240,nx[n],"a)",fontsize=30)
+    plt.text(sx[0]/dh[n] + 20, sy[0]/dh[n] + 10, "1", fontsize=20, fontweight="bold")    
+    plt.text(sx[1]/dh[n] - 50, sy[1]/dh[n] + 10, "2", fontsize=20, fontweight="bold")    
+    plt.text(sx[2]/dh[n] + 20, sy[2]/dh[n] - 20, "3", fontsize=20, fontweight="bold")    
+    plt.text(sx[3]/dh[n] - 50, sy[3]/dh[n] - 20, "4", fontsize=20, fontweight="bold")    
+    plt.text(sx[4]/dh[n] + 10, sy[4]/dh[n] + 10, "5", fontsize=20, fontweight="bold")    
 
-    # ax2 = plt.subplot(G[:2,-1:]) #-----------------------------------------------------   
-    # depth = np.arange(1100)
-    # vpWell = np.ones(1100) * 1.5
-    # vpWell[1000:] = 2.0
-    # plt.plot(vpWell, depth)
-    # plt.xlim([1,2.5])
-    # plt.ylim([0,1100])
-    # plt.title("Velocity model", fontsize=20)
-    # plt.xlabel("P wave velocity [km/s]", fontsize=17)
-    # plt.ylabel("Depth [m]", fontsize=17)
-    # plt.xticks(np.arange(1,3,0.5),np.arange(1,3,0.5))
-    # plt.gca().invert_yaxis()
-    # plt.text(0.5,0,"b)",fontsize=30)
+    ax2 = plt.subplot(G[:2,-1:]) #-----------------------------------------------------   
+    depth = np.arange(1100)
+    vpWell = np.ones(1100) * 1.5
+    vpWell[1000:] = 2.0
+    plt.plot(vpWell, depth)
+    plt.xlim([1,2.5])
+    plt.ylim([0,1100])
+    plt.title("Velocity model", fontsize=20)
+    plt.xlabel("P wave velocity [km/s]", fontsize=17)
+    plt.ylabel("Depth [m]", fontsize=17)
+    plt.xticks(np.arange(1,3,0.5),np.arange(1,3,0.5))
+    plt.gca().invert_yaxis()
+    plt.text(0.5,0,"b)",fontsize=30)
 
-    # ax3 = plt.subplot(G[2:3,:]) #-----------------------------------------------------
-    # plt.contour(times[:,:,xzPlane], levels=10)
-    # N = 5
-    # cbar = plt.colorbar(sm, cmap=cmap, ticks = np.linspace(0,np.max(times),N), aspect=10)
-    # cbar.set_label("Travel times [s]", fontsize=12)
-    # plt.imshow(model[:,:,xzPlane], aspect="auto",cmap="Greys",vmin=vmin, vmax=vmax)
-    # plt.title("XZ plane", fontsize=20)
-    # plt.ylabel("X axis [m]", fontsize=17)
-    # plt.xlabel("Z axis [m]", fontsize=17)
-    # plt.scatter(sx[n]/dh[n], sz[n]/dh[n], label = "Central shot", color = "k")
+    ax3 = plt.subplot(G[2:3,:]) #-----------------------------------------------------
+    plt.contour(times[:,:,xzPlane], levels=10)
+    N = 5
+    cbar = plt.colorbar(sm, cmap=cmap, ticks = np.linspace(0,np.max(times),N), aspect=10)
+    cbar.set_label("Travel times [s]", fontsize=12)
+    plt.imshow(model[:,:,xzPlane], aspect="auto",cmap="Greys",vmin=vmin, vmax=vmax)
+    plt.title("XZ plane", fontsize=20)
+    plt.ylabel("X axis [m]", fontsize=17)
+    plt.xlabel("Z axis [m]", fontsize=17)
+    plt.scatter(sx[n]/dh[n], sz[n]/dh[n], label = "Central shot", color = "k")
     
-    # plt.plot(np.ones(nz[n])*yzPlane,np.arange(nz[n]),"--g",label = "YZ plane projection")
-    # plt.plot(np.arange(nx[n]),np.ones(nx[n])*xyPlane,"--m",label = "XY plane projection")
-    # plt.legend(loc = "lower left", fontsize=8)
+    plt.plot(np.ones(nz[n])*yzPlane,np.arange(nz[n]),"--g",label = "YZ plane projection")
+    plt.plot(np.arange(nx[n]),np.ones(nx[n])*xyPlane,"--m",label = "XY plane projection")
+    plt.legend(loc = "lower left", fontsize=8)
 
-    # plt.xlim([-0.7,nx[n]-1])
-    # plt.ylim([-0.7,nz[n]-1])
+    plt.xlim([-0.7,nx[n]-1])
+    plt.ylim([-0.7,nz[n]-1])
 
-    # plt.xticks(xloc,xlab)
-    # plt.yticks(zloc,zlab)
-    # plt.gca().invert_yaxis()
+    plt.xticks(xloc,xlab)
+    plt.yticks(zloc,zlab)
+    plt.gca().invert_yaxis()
 
-    # plt.text(-150,0,"c)",fontsize=30)
+    plt.text(-150,0,"c)",fontsize=30)
     
-    # ax4 = plt.subplot(G[3:,:]) #-----------------------------------------------------
-    # plt.contour(times[:,yzPlane,:], levels=10)
-    # N = 5
-    # cbar = plt.colorbar(sm, cmap=cmap, ticks = np.linspace(0,np.max(times),N), aspect=15)
-    # cbar.set_label("Travel times [s]", fontsize=12)
-    # plt.imshow(model[:,yzPlane,:], aspect="auto",cmap="Greys",vmin=vmin, vmax=vmax)
-    # plt.title("YZ plane", fontsize=20)
-    # plt.ylabel("Y axis [m]", fontsize=17)
-    # plt.xlabel("Z axis [m]", fontsize=17)
-    # plt.scatter(sy[n]/dh[n], sz[n]/dh[n], label = "Central shot", color = "k")
+    ax4 = plt.subplot(G[3:,:]) #-----------------------------------------------------
+    plt.contour(times[:,yzPlane,:], levels=10)
+    N = 5
+    cbar = plt.colorbar(sm, cmap=cmap, ticks = np.linspace(0,np.max(times),N), aspect=15)
+    cbar.set_label("Travel times [s]", fontsize=12)
+    plt.imshow(model[:,yzPlane,:], aspect="auto",cmap="Greys",vmin=vmin, vmax=vmax)
+    plt.title("YZ plane", fontsize=20)
+    plt.ylabel("Y axis [m]", fontsize=17)
+    plt.xlabel("Z axis [m]", fontsize=17)
+    plt.scatter(sy[n]/dh[n], sz[n]/dh[n], label = "Central shot", color = "k")
     
-    # plt.plot(np.ones(nz[n])*xzPlane,np.arange(nz[n]),"--r",label = "XZ plane projection")
-    # plt.plot(np.arange(ny[n]),np.ones(ny[n])*xyPlane,"--m",label = "XY plane projection")
-    # plt.legend(loc = "lower left", fontsize=8)
+    plt.plot(np.ones(nz[n])*xzPlane,np.arange(nz[n]),"--r",label = "XZ plane projection")
+    plt.plot(np.arange(ny[n]),np.ones(ny[n])*xyPlane,"--m",label = "XY plane projection")
+    plt.legend(loc = "lower left", fontsize=8)
 
-    # plt.xlim([-0.7,ny[n]-1])
-    # plt.ylim([-0.7,nz[n]-1])
+    plt.xlim([-0.7,ny[n]-1])
+    plt.ylim([-0.7,nz[n]-1])
 
-    # plt.xticks(yloc,ylab)
-    # plt.yticks(zloc,zlab)
-    # plt.gca().invert_yaxis()
+    plt.xticks(yloc,ylab)
+    plt.yticks(zloc,zlab)
+    plt.gca().invert_yaxis()
 
-    # plt.text(-150,0,"d)",fontsize=30)
+    plt.text(-150,0,"d)",fontsize=30)
 
 
-    # plt.tight_layout()    
-    # plt.savefig("modelGeometry.png", dpi=200, bbox_inches="tight")
-
+    plt.tight_layout()    
+    plt.savefig("modelGeometry.png", dpi=200, bbox_inches="tight")
 
     #-----------------------------------------------------------------------------------------
     sId = np.array([1,3,2,4,5], dtype=int)
