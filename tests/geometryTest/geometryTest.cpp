06d7b758591223145a6feb8fb44d1471b00ee505
pbastosA <pbastos@id.uff.br>
2022-12-31 11:31:38 -0300

    geometry test updated

--- tests/geometryTest/geometryTest.cpp
+++ tests/geometryTest/geometryTest.cpp
@@ -1,6 +1,6 @@
 # include <iostream>
 
-# include "../../essentials/geometry.hpp"
+# include "../../src/essentials/geometry.hpp"
 
 int main(int argc, char**argv)
 {
--- tests/geometryTest/runGeometryTest.sh
+++ tests/geometryTest/runGeometryTest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-geometry="../../essentials/geometry.cpp"
-flags="-fopenmp -fast -acc -ta=tesla,cc60 -std=c++11 -g -lm"
+geometry="../../src/essentials/geometry.cpp"
+flags="-acc -fast -ta=tesla,cc60 -std=c++11 -g -lm"
 
 pgc++ $geometry geometryTest.cpp $flags -o geometryTest.exe
 
