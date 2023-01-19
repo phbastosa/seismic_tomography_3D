a3b185e5f1a57a73da4047aa84cff27a2986ea27
pbastosA <pbastos@id.uff.br>
2023-01-19 20:05:13 -0300

    auto picks working

--- main/analysis/3_automatic_picking.py
+++ main/analysis/3_automatic_picking.py
@@ -19,23 +19,23 @@ dt = 1e-3
 
 fw = 11
 
-for node in range(1): 
+# for node in range(nodes_all): 
 
-    rawPicks_all = readBinaryArray(shots_all, f"../../inputs/picks/rawPicks_shot_{node+1}_{shots_all}_samples.bin")
-    output_picks = np.zeros(shots_all)
+#     rawPicks_all = readBinaryArray(shots_all, f"../../inputs/picks/rawPicks_shot_{node+1}_{shots_all}_samples.bin")
+#     output_picks = np.zeros(shots_all)
 
-    for line in range(traces):
+#     for line in range(traces):
 
-        window = slice(int(line*shots_all/traces),int((line+1)*shots_all/traces))
+#         window = slice(int(line*shots_all/traces),int((line+1)*shots_all/traces))
 
-        rawPicks = rawPicks_all[window].copy()    
+#         rawPicks = rawPicks_all[window].copy()    
 
-        picks = median_filter(rawPicks, fw)
+#         rawPicks[int(fw/2):-int(fw/2)] = median_filter(rawPicks[int(fw/2):-int(fw/2)], fw)
 
-        output_picks[window] = savgol_filter(picks, fw, 2)     
+#         output_picks[window] = savgol_filter(rawPicks, fw, 2)     
 
-    print(f"File ../../inputs/picks/obsData_{shots_all}_samples_shot_{node+1}.bin was written successfully.")
-    output_picks.astype("float32", order = "F").tofile(f"../../inputs/picks/obsData_{shots_all}_samples_shot_{node+1}.bin")
+#     print(f"File ../../inputs/picks/obsData_{shots_all}_samples_shot_{node+1}.bin was written successfully.")
+#     output_picks.astype("float32", order = "F").tofile(f"../../inputs/picks/obsData_{shots_all}_samples_shot_{node+1}.bin")
 
 stop = timeit.default_timer()
 print('Run time: ', stop - start)  
@@ -55,18 +55,17 @@ tlab = np.around(np.linspace(0, updated_nt-1, 11) * dt, decimals = 1)
 xloc = np.linspace(0, traces-1, 11, dtype = int)
 xlab = np.linspace(0, traces, 11, dtype = int)
 
-for node in range(1):
+for node in range(nodes_all):
 
-    seismic_all = readBinaryMatrix(nt, shots_all, f"../../inputs/seismograms/seismogram_{nt}x{shots_all}_shot_{node+1}.bin")
     rawPicks_all = readBinaryArray(shots_all, f"../../inputs/picks/rawPicks_shot_{node+1}_{shots_all}_samples.bin")
     output_picks = readBinaryArray(shots_all, f"../../inputs/picks/obsData_{shots_all}_samples_shot_{node+1}.bin")
 
-    scale = 0.1 * np.std(seismic_all)
-
-    plt.figure(1, figsize = (18, 5))
-    plt.imshow(seismic_all[times, :], aspect = "auto", cmap = "Greys", vmin = -scale, vmax = scale)
+    plt.figure(1, figsize = (20, 5))
     plt.plot(rawPicks_all/dt, "o")
     plt.plot(output_picks/dt, "o")
     plt.title(f"{node+1}")
+    plt.xlim([0, shots_all])
+    plt.gca().invert_yaxis()
+    plt.tight_layout()
     plt.show()
 
